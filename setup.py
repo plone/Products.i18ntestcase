541240b148bd4cd9c6f7e82cd21fc5329ce5847f
Hanno Schlichting <hanno@hannosch.eu>
2008-01-01 20:03:08 +0000

    Docs reshuffling, prepare release
    svn path=/Products.i18ntestcase/trunk/; revision=56323

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ version = '1.2'
 
 setup(name='Products.i18ntestcase',
       version=version,
-      description="Products.i18ntestcase makes it easier to use "
-                  "QueueCatalog in your Plone site.",
+      description="Products.i18ntestcase is build on top of the ZopeTestCase "
+                  "package. It has been developed to simplify testing of "
+                  "gettext i18n files for Zope products.",
       long_description="""\
       """,
       classifiers=[
@@ -14,7 +15,7 @@ setup(name='Products.i18ntestcase',
         "Framework :: Zope2",
         "Programming Language :: Python",
       ],
-      keywords='Plone i18n testcase',
+      keywords='Plone i18n gettext testcase',
       author='Hanno Schlichting',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://svn.plone.org/svn/collective/Products.i18ntestcase/trunk',
