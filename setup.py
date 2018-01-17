fadbb07dafe63588dc069c50f46327bac9c7657b
Maurits van Rees <maurits@vanrees.org>
2018-01-17 12:28:13 +0100

    Removed Plone 5.1 from classifiers because we do not use it there.
    [ci skip]

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
         "Framework :: Zope2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
